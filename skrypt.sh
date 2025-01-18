ee527d7e7274df576b1a9b373d388b550e2f9fc9
Monika <s32719@pjwstk.edu.pl>
2025-01-18 10:25:24 +0100

    zadanie 1

---
+++ skrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+parametr1=$1
+parametr2=$2
+parametr3=$3
+
+mkdir 'Kartki Świąteczne' 'Pozostałe' 'Prace domowe' 'Zadania'
+cd 'Kartki Świąteczne'
+mkdir Kotki Pieski
+cd ../Pozostałe
+mkdir 'Listy zakupów' Przepisy
+cd 'Listy zakupów'
+mkdir 'Święta 2022' 'Święta 2023'
+cd ../..
